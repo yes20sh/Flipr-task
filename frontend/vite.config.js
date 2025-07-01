422befabec9dab24b545dc5f8b0d60d957cdf02b
Yashraj Singh Solanki <engineeryashsingh@gmail.com>
2025-07-01 17:27:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   preview: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 4173
+    port: parseInt(process.env.PORT) || 4173,
+    allowedHosts: ['flipr-task-yashraj.onrender.com'] 
   }
-})
+});
