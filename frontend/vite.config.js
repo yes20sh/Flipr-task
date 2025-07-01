17f300cae27377edbf90d5fa0b578641c2626175
Yashraj Singh Solanki <engineeryashsingh@gmail.com>
2025-07-01 17:23:06 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173
+  }
 })
